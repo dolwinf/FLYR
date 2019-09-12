cc555a30287fd8b18136df289f4956029b3fc849
Dolwin <dolwinf@gmail.com>
2019-09-12 20:55:56 +1000

    Added variables to query string

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,19 @@
+var placeFrom = "SYD";
+var placeTo = "NYC";
+var dateFrom = "18/11/2019";
+var dateTo = "20/11/2019";
+
 $.ajax({
   url:
-    "https://api.skypicker.com/flights?flyFrom=PRG&to=LGW&dateFrom=18/11/2019&dateTo=12/12/2019&limit=10&partner=picky",
+    "https://api.skypicker.com/flights?flyFrom=" +
+    placeFrom +
+    "&to=" +
+    placeTo +
+    "&dateFrom=" +
+    dateFrom +
+    "&dateTo=" +
+    dateTo +
+    "&limit=10&partner=picky",
   method: "GET"
 }).then(function(response) {
   console.log(response);
