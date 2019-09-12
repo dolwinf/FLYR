d1ee665a5067d281916f51a2445b55c5cf40fffa
Dolwin Fernandes <dolwinf@gmail.com>
2019-09-12 16:46:18 +1000

    Changes dates on the query string url for a valid response from the api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 $.ajax({
   url:
-    "https://api.skypicker.com/flights?flyFrom=PRG&to=LGW&dateFrom=18/11/2018&dateTo=12/12/2018&partner=picky",
+    "https://api.skypicker.com/flights?flyFrom=PRG&to=LGW&dateFrom=18/11/2019&dateTo=12/12/2019&partner=picky",
   method: "GET"
 }).then(function(response) {
   console.log(response);
